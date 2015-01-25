39ec77554cbbf0056c78e97080a9d47caacf968f
Tiago Sandes Fernandes <tsandesfernandes@gmail.com>
2015-01-25 12:47:40 -0200

    Initial Commit

---
+++ Where am I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where am I
+//
+//  Created by Tiago Sandes Fernandes on 25/01/15.
+//  Copyright (c) 2015 Tiago Sandes Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
